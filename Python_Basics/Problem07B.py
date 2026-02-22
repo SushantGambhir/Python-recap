c9ff0df1dceec630c7aa362ae5ecbd5c6310258c
Sushant Gambhir <gambhirboy1234@gmail.com>
2026-02-22 11:29:44 +0530

    files renamed

--- Python_Basics/Problem07B.py
+++ Python_Basics/Problem07B.py
@@ -1,9 +1,9 @@
-import Problem7A
+import Python_Basics.Problem07A as Problem07A
 
 p=float(input("Enter the amount: "))
 r=float(input("Enter the rate: "))
 t=float(input("Enter the number of years: "))
 
-interest= Problem7A.compound(p,r,t)
+interest= Problem07A.compound(p,r,t)
 
 print("Compound Interest = ",interest)
