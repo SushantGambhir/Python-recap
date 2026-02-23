f7eab89f6595fda80123207c471e8645079f9c95
Sushant Gambhir <sushantgambhir9878@gmail.com>
2026-02-23 22:23:37 +0530

    Problem19

---
+++ Python_Basics/Problem19.py
@@ -0,0 +1,9 @@
+# Check if a value 200 exists in a dictionary 
+# d1 = {'a': 100, 'b': 200, 'c': 300}
+
+d1 = {'a': 100, 'b': 200, 'c': 300}
+
+if 200 in d1.values():
+    print("200 exists in the dictionary")
+else:
+    print("200 does not exist in the dictionary")
